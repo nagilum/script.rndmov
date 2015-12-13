b6d34223d2e052e05e10a3ebd8584d09bffbad64
Stian Hanger <pdnagilum@gmail.com>
2015-12-13 03:44:45 +0100

    Initial commit

---
+++ addon.py
@@ -0,0 +1,19 @@
+﻿import xbmc,xbmcaddon,xbmcgui,json,random
+
+def getAllMovies():
+  rpccmd = {'jsonrpc': '2.0', 'method': 'VideoLibrary.GetMovies', 'params': { 'properties': [ 'file' ] }, 'id': 'libMovies'}
+  rpccmd = json.dumps(rpccmd)
+  result = xbmc.executeJSONRPC(rpccmd)
+  result = json.loads(result)
+  return result
+
+addon     = xbmcaddon.Addon()
+addonName = addon.getAddonInfo('name')
+addonIcon = addon.getAddonInfo('icon')
+
+movies = getAllMovies()
+movie  = random.choice(movies['result']['movies'])
+time   = 5000
+
+xbmc.executebuiltin('PlayMedia(%s)'%(movie['file']))
+xbmc.executebuiltin('Notification(%s, %s %s, %d, %s)'%(addonName,"Playing ",movie['label'],time,addonIcon))
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Random Movie
+
+A Kodi script add-on to play a random movie from your movies library.
